87b598126647fde1efbd8a6bc54c875f892fe23e
elWildo <francescowild90.com>
2024-10-16 16:51:42 +0100

    #3 - Update RandomMessageElement
    - Add vocabulary to utils
    - Add random int function to utils
    - Add Pixi tunnel out
    - Add RandomMessageElement Implementation
    - Add RandomMessageElement style

--- src/components/RandomMessageElement.tsx
+++ src/components/RandomMessageElement.tsx
@@ -1,3 +1,34 @@
+import { Assets, Resource, Spritesheet, Texture } from "pixi.js";
+import cardSpirtesheets from "/spritesheets/spritesheetcollection.json?url";
+import cardSpirtesheetsImg from "/spritesheets/spritesheetcollection.png?url";
+import { pokemonArray as vocabulary, randomInt } from "../utils_hooks/utils";
+
 export default function RandomMessageElement() {
-  return <div>{"Random stuff"}</div>;
+  const spriteSheet = Assets.get<Spritesheet>(cardSpirtesheets);
+  const imgNameList = Object.keys(spriteSheet.data.frames);
+  const totImages = imgNameList.length;
+  const randomFontSize = randomInt(12, 48);
+  const type: "text" | "icon" = randomInt(0, 1) == 0 ? "text" : "icon";
+  const randomText = vocabulary[randomInt(0, vocabulary.length - 1)];
+  const randomImgName = imgNameList[randomInt(0, totImages - 1)];
+  const randomImg = Assets.get<Texture<Resource>>(randomImgName);
+
+  switch (type) {
+    case "text":
+      return <div style={{ fontSize: randomFontSize }}>{randomText}</div>;
+    case "icon":
+      return (
+        <div
+          style={{
+            backgroundImage: `url(${cardSpirtesheetsImg})`,
+            width: randomImg.frame.width,
+            height: randomImg.frame.height,
+            backgroundPositionX: randomImg.frame.x,
+            backgroundPositionY: -randomImg.frame.y,
+          }}
+        />
+      );
+    default:
+      return null;
+  }
 }
