0fbcddb49c19e72629e9915754095e881dab78b7
elWildo <francescowild90.com>
2024-10-17 15:39:41 +0100

    #6 - Init Task 3
    - Move loading assets into FireEffect
    - Update emitter to use correct asset

--- src/components/FXComponent.tsx
+++ src/components/FXComponent.tsx
@@ -1,142 +1,145 @@
 import { PixiComponent } from "@pixi/react";
-import { Assets, Container, Texture } from "pixi.js";
-import fx_setting from "/spritesheets/default-bundle.json?url";
-import fx_spritesheet from "/spritesheets/revoltfx-spritesheet.json?url";
+import {
+  Application,
+  Assets,
+  Container,
+  ICanvas,
+  Resource,
+  Texture,
+} from "pixi.js";
 import { Emitter } from "@pixi/particle-emitter";
-
-const img = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png";
-
-const emitterConfig = {
-  lifetime: {
-    min: 0.5,
-    max: 0.5,
-  },
-  frequency: 0.008,
-  spawnChance: 1,
-  particlesPerWave: 1,
-  emitterLifetime: 0.31,
-  maxParticles: 1000,
-  pos: {
-    x: 0,
-    y: 0,
-  },
-  addAtBack: false,
-  behaviors: [
-    {
-      type: "alpha",
-      config: {
-        alpha: {
-          list: [
-            {
-              value: 0.8,
-              time: 0,
-            },
-            {
-              value: 0.1,
-              time: 1,
-            },
-          ],
-        },
-      },
-    },
-    {
-      type: "scale",
-      config: {
-        scale: {
-          list: [
-            {
-              value: 1,
-              time: 0,
-            },
-            {
-              value: 0.3,
-              time: 1,
-            },
-          ],
-        },
-      },
-    },
-    {
-      type: "color",
-      config: {
-        color: {
-          list: [
-            {
-              value: "fb1010",
-              time: 0,
-            },
-            {
-              value: "f5b830",
-              time: 1,
-            },
-          ],
-        },
-      },
-    },
-    {
-      type: "moveSpeed",
-      config: {
-        speed: {
-          list: [
-            {
-              value: 200,
-              time: 0,
-            },
-            {
-              value: 100,
-              time: 1,
-            },
-          ],
-          isStepped: false,
-        },
-      },
-    },
-    {
-      type: "rotationStatic",
-      config: {
-        min: 0,
-        max: 360,
-      },
-    },
-    {
-      type: "spawnShape",
-      config: {
-        type: "torus",
-        data: {
-          x: 0,
-          y: 0,
-          radius: 10,
-        },
-      },
-    },
-    {
-      type: "textureSingle",
-      config: {
-        texture: Texture.from(img),
-      },
-    },
-  ],
-};
 
 interface ConteinerWithEmitter extends Container {
   emitter?: Emitter;
 }
 
 const FXComponent = PixiComponent<
-  { [key: string]: unknown },
+  {
+    app: Application<ICanvas>;
+    [key: string]: unknown;
+  },
   ConteinerWithEmitter
 >("Emitter", {
-  create() {
-    Assets.load([
-      { alias: "fx_settings", src: fx_setting },
-      { alias: "fx_settings", src: fx_spritesheet },
-      { alias: "img_test", src: img },
-    ]);
-    return new Container();
+  create(props) {
+    const { app } = props;
+    const container = new Container();
+    container.x = app.screen.width / 2;
+    container.y = app.screen.height / 2;
+    return container;
   },
   applyProps(instance) {
     if (!instance.emitter) {
-      // const texture = Assets.get("img_test");
+      const emitterConfig = {
+        lifetime: {
+          min: 0.5,
+          max: 0.5,
+        },
+        frequency: 0.008,
+        spawnChance: 1,
+        particlesPerWave: 1,
+        emitterLifetime: Infinity,
+        maxParticles: 1000,
+        pos: {
+          x: 0,
+          y: 0,
+        },
+        addAtBack: false,
+        behaviors: [
+          {
+            type: "alpha",
+            config: {
+              alpha: {
+                list: [
+                  {
+                    value: 0.8,
+                    time: 0,
+                  },
+                  {
+                    value: 0.1,
+                    time: 1,
+                  },
+                ],
+              },
+            },
+          },
+          {
+            type: "scale",
+            config: {
+              scale: {
+                list: [
+                  {
+                    value: 1,
+                    time: 0,
+                  },
+                  {
+                    value: 0.3,
+                    time: 1,
+                  },
+                ],
+              },
+            },
+          },
+          {
+            type: "color",
+            config: {
+              color: {
+                list: [
+                  {
+                    value: "fb1010",
+                    time: 0,
+                  },
+                  {
+                    value: "f5b830",
+                    time: 1,
+                  },
+                ],
+              },
+            },
+          },
+          {
+            type: "moveSpeed",
+            config: {
+              speed: {
+                list: [
+                  {
+                    value: 200,
+                    time: 0,
+                  },
+                  {
+                    value: 100,
+                    time: 1,
+                  },
+                ],
+                isStepped: false,
+              },
+            },
+          },
+          {
+            type: "rotationStatic",
+            config: {
+              min: 0,
+              max: 360,
+            },
+          },
+          {
+            type: "spawnShape",
+            config: {
+              type: "torus",
+              data: {
+                x: 0,
+                y: 0,
+                radius: 10,
+              },
+            },
+          },
+          {
+            type: "textureSingle",
+            config: {
+              texture: Assets.get<Texture<Resource>>("fx-flame01"),
+            },
+          },
+        ],
+      };
       instance.emitter = new Emitter(instance, emitterConfig);
       let elapsed = Date.now();
 
--- src/games/FireEffect.tsx
+++ src/games/FireEffect.tsx
@@ -1,6 +1,8 @@
 import { useApp } from "@pixi/react";
 import FXComponent from "../components/FXComponent";
-import { Application, ICanvas } from "pixi.js";
+import { Application, Assets, ICanvas } from "pixi.js";
+import { useEffect, useState } from "react";
+import fx_spritesheet from "/spritesheets/revoltfx-spritesheet.json?url";
 
 export default function FireEffect() {
   const app = useApp();
@@ -9,5 +11,19 @@ export default function FireEffect() {
   ).__PIXI_DEVTOOLS__ = {
     app: app,
   };
-  return <FXComponent />;
+  const [loaded, SetLoaded] = useState(false);
+
+  useEffect(() => {
+    const load = async () => {
+      const assets = await Assets.cache.has(fx_spritesheet);
+      if (!assets)
+        await Assets.load(fx_spritesheet).then(() => SetLoaded(true));
+    };
+    load();
+    return () => Assets.reset();
+
+    //It's loading it 2 times and i dont understand why
+  }, []);
+
+  return loaded && <FXComponent app={app} />;
 }
